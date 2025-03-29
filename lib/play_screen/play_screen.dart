74c06615ff44b0754eed0ecc9964f0c6118edd16
infwrw879 <ahmad.it@outlook.de>
2025-03-29 01:07:05 +0100

    spielfeld mit zahlen gefüllt

--- lib/play_screen/play_screen.dart
+++ lib/play_screen/play_screen.dart
@@ -1,80 +1,77 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class PlayScreen extends StatelessWidget {
+class PlayScreen extends StatefulWidget {
   const PlayScreen({super.key});
+
+  @override
+  State<PlayScreen> createState() => _PlayScreenState();
+}
+
+class _PlayScreenState extends State<PlayScreen> {
+  final List<int> buttonsNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int? selectedFieldIndex;
+  List<int> solutions = List.generate(81, (index) => Random().nextInt(10));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       // Äußere Raster
-      body: Column(
-        children: [
-          Expanded(
-            flex: 8,
-            child: ScrollConfiguration(
-              behavior: ScrollConfiguration.of(
-                context,
-              ).copyWith(scrollbars: false),
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                ),
-                itemCount: 9,
-                itemBuilder: (context, outerBoxIndex) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                    // Innere Ratser
-                    child: GridView.builder(
-                      // shrinkWrap: true,
-                      // physics: NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                      ),
-                      itemCount: 9,
-                      itemBuilder: (context, innerBoxIndex) {
-                        int index = (outerBoxIndex * 9) + innerBoxIndex;
-                        return GestureDetector(
-                          onTap: () {
-                            print(index.toString());
-                          },
-                          child: SizedBox.expand(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.grey,
-                                  width: 1,
-                                ),
-                              ),
-                              child: Text(index.toString()),
-                            ),
-                          ),
-                        );
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 8,
+              child: ScrollConfiguration(
+                behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 9),
+                  itemCount: 81,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedFieldIndex == index ? selectedFieldIndex = null : selectedFieldIndex = index;
+                        });
                       },
-                    ),
-                  );
-                },
+                      child: SizedBox.expand(
+                        child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(color: selectedFieldIndex == index ? Colors.blue[200] : null),
+                          child: Text(solutions[index] == 0 ? '' : solutions[index].toString()),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: List.generate(9, (index) {
-                return Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(index.toString()),
-                  ),
-                );
-              }),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(10, (idx) {
+                  return Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (selectedFieldIndex != null) {
+                          setState(() {
+                            solutions[selectedFieldIndex!] = buttonsNumbers[idx];
+                          });
+                        }
+                      },
+                      child: Text(buttonsNumbers[idx].toString()),
+                    ),
+                  );
+                }),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
